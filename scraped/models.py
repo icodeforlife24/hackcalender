73cb3112bce4c5046271ba110735376c3deefcfa
icodeforlife24 <shiviutkarsh@gmail.com>
2025-05-10 15:23:38 +0530

    MODEL CREATED

--- scraped/models.py
+++ scraped/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class contests:
+    name = models.CharField(max_length=100)
+    start_time = models.DateField()
+    duration = models.CharField(max_length=100)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
